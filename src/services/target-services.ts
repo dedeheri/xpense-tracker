7d05ba8287d1b13d17b496754c0dbcbc561dc2ab
Dede Heri <dedeherii60@gmail.com>
2025-12-18 00:52:37 +0700

    [improve] : services and design

--- src/services/target-services.ts
+++ src/services/target-services.ts
@@ -1,21 +1,21 @@
 import prisma from "@/lib/prisma";
 import { Prisma } from "@prisma/client";
 
-const findMany = async (options?: Prisma.TargetFindManyArgs) => {
+const findMany = async (options?) => {
   const results = await prisma.target.findMany({
     ...options,
   });
   return results;
 };
 
-const findFirst = async (options: Prisma.TargetFindFirstArgs) => {
+const findFirst = async (options) => {
   const results = await prisma.target.findFirst({
     ...options,
   });
   return results;
 };
 
-const createOne = async (options: Prisma.TargetCreateArgs) => {
+const createOne = async (options) => {
   const results = await prisma.target.create({ ...options });
   return results;
 };
